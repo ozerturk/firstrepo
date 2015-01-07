72db4d9f50b4f4b5325306f8cd37ab1b731c22ae
eozeert <ozer.erturk@ericsson.com>
2015-01-07 09:48:41 +0200

    modified the TestClass

--- src/com/ozer/tester/TestClass.java
+++ src/com/ozer/tester/TestClass.java
@@ -2,7 +2,8 @@ package com.ozer.tester;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Here is the initial committed class");
+		System.out.println("Here is the more committed class");
+		System.out.println("Here is the hello committed class");
 	}
 
 }

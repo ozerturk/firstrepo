18456eba0b843b1989c1028535a8c8fb3240713a
oerturk <ozer.erturk@gmail.com>
2015-01-08 10:48:06 +0200

    Fix for issue SAP-19

--- src/com/ozer/tester/TestClass.java
+++ src/com/ozer/tester/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Here is the more committed class");
 		System.out.println("Here is the hello committed class");
+		System.out.println("Initial fix commit");
 	}
 
 }

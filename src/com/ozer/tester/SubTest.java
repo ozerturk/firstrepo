33961c9b1944e51f22ec303de5b490a907f25ab4
oerturk <ozer.erturk@gmail.com>
2015-01-09 11:06:00 +0200

    conflict resolved

---
+++ src/com/ozer/tester/SubTest.java
@@ -0,0 +1,9 @@
+package com.ozer.tester;
+
+public class SubTest extends TestClass{
+
+	public static void main(String[] args) {
+		SubTest subtest = new SubTest();
+		subtest.sayMyName();
+	}
+}
--- src/com/ozer/tester/TestClass.java
+++ src/com/ozer/tester/TestClass.java
@@ -3,6 +3,15 @@ package com.ozer.tester;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Huloooooogggg");
+		System.out.println("Here is the more committed class");
+		System.out.println("Here is my Hello committed class");
+		System.out.println("Initial fix commit");
+		System.out.println("git test lets see the merge working");
+		System.out.println("git test lets see the merge working");
+	}
+	
+	public void sayMyName(){
+		System.out.println(this.getClass().getName());
 	}
 
 }

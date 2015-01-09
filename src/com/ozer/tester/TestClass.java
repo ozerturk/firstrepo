7fb6f0d23d013dd956a8a78e2ff39570fb012dce
oerturk <ozer.erturk@gmail.com>
2015-01-09 10:15:28 +0200

    dfsd merge it

--- src/com/ozer/tester/TestClass.java
+++ src/com/ozer/tester/TestClass.java
@@ -2,9 +2,7 @@ package com.ozer.tester;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Here is the more committed class");
-		System.out.println("Here is the hello committed class");
-		System.out.println("Initial fix commit");
+		System.out.println("Huloooooogggg");
 	}
 
 }
